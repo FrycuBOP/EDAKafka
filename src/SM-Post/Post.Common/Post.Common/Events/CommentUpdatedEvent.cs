5795b477f14c32fc526367dfea25177b978a4aac
Mateusz Frydrych <mat.frydrych.92@gmail.com>
2024-09-17 13:40:47 +0200

    Initial Commit

---
+++ src/SM-Post/Post.Common/Post.Common/Events/CommentUpdatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentUpdatedEvent : BaseEvent
+    {
+        public CommentUpdatedEvent() : base(nameof(CommentAddedEvent))
+        {
+        }
+
+        public Guid CommentId { get; set; }
+        public required string Comment { get; set; }
+        public required string UserName { get; set; }
+        public DateTime EditDate { get; set; }
+    }
+}
