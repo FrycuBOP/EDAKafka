8af40fac42859b840457e2ab3fb4bfd50feff9ab
Mateusz Frydrych <mat.frydrych.92@gmail.com>
2024-09-17 14:58:46 +0200

    Event Model

--- src/CQRS-ES/CQRS.Core/Events/EventModel.cs
+++ src/CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace CQRS.Core.Events
 {
-    internal class EventModel
+    public class EventModel
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public required string Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Guid AggregateIdentifier { get; set; }
+        public required string AggreagateType { get; set; }
+        public int Version { get; set; }
+        public required string EventType { get; set; }
+        public required BaseEvent EventData { get; set; }
     }
 }
